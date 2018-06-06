490929d1577f383ea55a6099641d984ce1ba6701
administrator <liquidnuker@users.noreply.github.com>
2018-06-06 10:22:48 -0700

    initial commit

---
+++ src/vue-components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="row no-gutters">
+    <div class="col-sm-3">Home.vue</div>
+    <div class="col-sm-9">39</div>
+  </div>
+</template>
+<script>
+  export default {
+    data () {
+      return {    
+      }
+    },
+    mounted: function () {
+      console.log("Home-test.vue mounted");
+    }
+  }
+</script>
