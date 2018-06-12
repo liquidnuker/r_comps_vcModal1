d6a9f8fbd65bd0c63b133bfc4b43811e5e455894
administrator <liquidnuker@users.noreply.github.com>
2018-06-12 10:59:53 -0700

    added svg prev/next icons

--- src/vue-components/vcModal2.vue
+++ src/vue-components/vcModal2.vue
@@ -20,19 +20,23 @@
           </button>
         </div>
         <div class="bs4modal-body">
-        <div class="vcmodal2_prevholder">
-          <span>zz</span>
-        </div>
+          <div class="vcmodal2_prevholder">
+            <div class="vcmodal2_svgholder">
+             <svg xmlns="http://www.w3.org/2000/svg"><path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"/></svg>
+            </div>
+          </div>
           <p>modal content...
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
           proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           <div class="vcmodal2_nextholder">
-          <span>zz</span>
-        </div>
+            <div class="vcmodal2_svgholder">
+              <svg xmlns="http://www.w3.org/2000/svg"><path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"/></svg>
+            </div>
+          </div>
         </div>
         <div class="bs4modal-footer">
           <button type="button" class="btn btn1-01" data-dismiss="bs4modal" @click="toggleModal(false)">Close</button>
